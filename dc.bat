dee70d02f06c1c24aa2be24165d84a8d71ef9c6d
Napi <ibnuyahya@gmail.com>
2020-06-16 21:33:39 +0800

    first commit

---
+++ dc.bat
@@ -0,0 +1,9 @@
+:: short comment utk docker
+:: penggunaan
+:: 	.\dc NAMA_COMMAND
+:: contoh
+::      .\dc php artisan
+
+docker exec -it -w /var/www/projects/rnd/tenancy php7.fpm %*
+@echo:
+@echo:
